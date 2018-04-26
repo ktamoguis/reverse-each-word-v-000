a9de21a56f242544aac03caea06b5143343872f1
Karl Amoguis <ktamoguis@gmail.com>
2018-04-26 10:03:07 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,6 @@
 def reverse_each_word(string)
   newstring = ""
   arraystring = string.split(" ")
-  arraystring.each do |words|
-    newstring << words.reverse << " "
-  end
-  newstring.strip
-end
-
-def reverse_each_word(string)
-  newstring = ""
-  arraystring = string.split(" ")
   arraystring.collect do |words|
     newstring << words.reverse << " "
   end
